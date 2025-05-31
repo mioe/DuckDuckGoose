d9bbb8dcfeec6bae1c0f06475eee2d1a318497f6
mioe <mioe.boss@gmail.com>
2025-05-31 19:20:09 +0300

    init overlay

--- DuckDuckGoose/Playground/DeadlineView/DeadlineEvent.swift
+++ DuckDuckGoose/Playground/DeadlineView/DeadlineEvent.swift
@@ -9,36 +9,60 @@ import SwiftUI
 
 struct DeadlineEvent: View {
 	var body: some View {
-		ZStack(alignment: .leading) {
-			CategoryLine()
-				.fill(.purple)
-				.frame(width: 40)
+		VStack {
 			VStack {
-				VStack {
-					HStack(alignment: .top) {
-						Image(systemName: "screwdriver")
-						VStack(alignment: .leading) {
-							Text("Fix token refresh issue")
-								.fontWeight(.semibold)
-							Text("11:10–12:45")
-								.font(.subheadline)
-						}
-						Spacer()
-						Image(systemName: "clock")
+				HStack(alignment: .top) {
+					Image(systemName: "screwdriver")
+					VStack(alignment: .leading) {
+						Text("Fix token refresh issue")
+							.fontWeight(.semibold)
+						Text("11:10–12:45")
+							.font(.subheadline)
 					}
+					Spacer()
+					Image(systemName: "clock")
 				}
-				.padding()
-				.background(.regularMaterial, in: RoundedRectangle(cornerRadius: 16))
-				.shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
-			}.padding(.leading, 4)
+			}
+			.padding()
+			.background(.regularMaterial, in: RoundedRectangle(cornerRadius: 16))
+			.shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
 		}
+		.padding(.leading, 4)
+		.overlay {
+			ZStack {
+				LeftMark()
+			}
+		}
+	}
+	
+	@ViewBuilder
+	private func LeftMark() -> some View {
+		VStack {
+			CategoryLine()
+				.fill(.green)
+				.clipShape(RoundedRectangle(cornerRadius: 16))
+		}.padding(.vertical, 4)
 	}
 	
 	private struct CategoryLine: Shape {
 		func path(in rect: CGRect) -> Path {
-			let cornerRadius: CGFloat = 10
-			let cgPath = CGPath(roundedRect: rect, cornerWidth: cornerRadius, cornerHeight: cornerRadius, transform: nil)
-			return Path(cgPath)
+			var p = Path()
+			
+			p.move(to: CGPoint(x: 0, y: 0))
+			p.addLine(to: CGPoint(x: rect.width * 0.6, y: 0))
+			
+			p.addArc(
+				center: CGPoint(x: rect.width * 0.1, y: rect.height / 2),
+				radius: rect.height / 2,
+				startAngle: .degrees(-90),
+				endAngle: .degrees(90),
+				clockwise: false
+			)
+			
+			p.addLine(to: CGPoint(x: 0, y: rect.height))
+			p.closeSubpath()
+			
+			return p
 		}
 	}
 }

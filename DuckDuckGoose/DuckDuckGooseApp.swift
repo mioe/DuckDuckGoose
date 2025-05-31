9d9b1a67c03f7ae418d366be67368ee50821d221
mioe <mioe.boss@gmail.com>
2025-05-31 10:26:46 +0300

    init

---
+++ DuckDuckGoose/DuckDuckGooseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuckDuckGooseApp.swift
+//  DuckDuckGoose
+//
+//  Created by misha gezha on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct DuckDuckGooseApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
